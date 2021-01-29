f60d284f9231bccdc2cd2df5d55a6b91dc123d51
Angel <albertoangelaguilera@gmail.con>
2021-01-29 12:39:21 -0600

    Fixed some typos in test suites

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,46 +1,38 @@
 import mapValue from './app';
 describe('Testing special test cases only', () => { 
       it('3 should output  Music', () => {
-        const consoleLog = console.log;
-        console.log = jest.fn();
+
         expect(mapValue(3)).toBe("Music");
         
       });
       it('9 should output  Music', () => {
-        const consoleLog = console.log;
-        console.log = jest.fn();
+       
         expect(mapValue(9)).toBe("Music");
         
       });
     
         it('5 should output IT', () => {
-        const consoleLog = console.log;
-        console.log = jest.fn();
+        
           expect(mapValue(5)).toBe("IT");
         
         });
         it('50 should output IT', () => {
-            const consoleLog = console.log;
-            console.log = jest.fn();
+            
               expect(mapValue(50)).toBe("IT");
             
             });
         it('15 should output Musical', () => {
-        const consoleLog = console.log;
-        console.log = jest.fn();
+        
             expect(mapValue(15)).toBe("Musical");
           
         });
         it('45 should output Musical', () => {
-            const consoleLog = console.log;
-            console.log = jest.fn();
                 expect(mapValue(45)).toBe("Musical");
               
             });
      
        it('1 should output the number itself', () => {
-        const consoleLog = console.log;
-        console.log = jest.fn();
+
             expect(mapValue(1)).toBe(1);
         
           });
